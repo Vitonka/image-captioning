b5dd476e0c1c6515dc14d9e1513f67531a4f340e
vitonka <victor.otliga@gmail.com>
2020-11-14 11:14:39 +0300

    Script for dataset loading added

---
+++ datasets/coco.sh
@@ -0,0 +1,27 @@
+mkdir coco
+cd coco
+mkdir images
+cd images
+
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
+wget http://images.cocodataset.org/zips/test2014.zip
+
+unzip train2014.zip
+unzip val2014.zip
+unzip test2014.zip
+
+rm train2014.zip
+rm val2014.zip
+rm test2014.zip
+
+
+cd ../
+wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+wget http://images.cocodataset.org/annotations/image_info_test2014.zip
+
+unzip annotations_trainval2014.zip
+unzip image_info_test2014.zip
+
+rm annotations_trainval2014.zip
+rm image_info_test2014.zip
