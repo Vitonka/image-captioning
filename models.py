d9903a2b223b2ae64586db542890486e95cc08ae
vitonka <victor.otliga@gmail.com>
2020-12-06 14:54:12 +0000

    Add model with pretrained image vectors + LSTM decoder model

--- models.py
+++ models.py
@@ -56,7 +56,7 @@ class SimpleModelWithEncoder(nn.Module):
 
         self.hidden_size = hidden_size
         self.embedding = nn.Embedding(num_embeddings=dict_size, embedding_dim=embedding_dim)
-        self.rnn = nn.RNN(input_size=embedding_dim, hidden_size=hidden_size)
+        self.rnn = nn.RNN(input_size=embedding_dim, hidden_size=hidden_size, batch_first=True)
         self.linear2 = nn.Linear(in_features=hidden_size, out_features=dict_size)
 
     def encoder(self, image):
@@ -75,3 +75,72 @@ class SimpleModelWithEncoder(nn.Module):
         image_vector = self.encoder(image)
         image_vector = image_vector.unsqueeze(0)
         return self.decoder(image_vector, input_captions)
+
+class SimpleModelWithPreptrainedImageEmbeddings(nn.Module):
+    def __init__(self, dict_size, embedding_dim, hidden_size, *args, **kwargs):
+        super(SimpleModelWithPreptrainedImageEmbeddings, self).__init__(*args, **kwargs)
+        # TODO: try to use mean instead of flatten all the features
+#        self.linear1 = nn.Linear(in_features=100352, out_features=hidden_size)
+        self.linear1 = nn.Linear(in_features=2048, out_features=hidden_size)
+
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(num_embeddings=dict_size, embedding_dim=embedding_dim)
+        self.rnn = nn.RNN(input_size=embedding_dim, hidden_size=hidden_size, nonlinearity='relu')
+        self.linear2 = nn.Linear(in_features=hidden_size, out_features=dict_size)
+
+        self._eval_mode = False
+
+    def encoder(self, image):
+        image = image.mean(dim=(2, 3))
+        return self.linear1(image).view(-1, self.hidden_size)
+#        return self.linear1(image.view(image.shape[0], -1)).view(-1, self.hidden_size)
+
+    def decoder(self, image_vector, input_captions):
+        embeddings = nn.utils.rnn.PackedSequence(
+            self.embedding(input_captions.data),
+            input_captions.batch_sizes)
+        decoded, hiddens = self.rnn(embeddings, image_vector)
+        probs = self.linear2(decoded.data)
+        return nn.utils.rnn.PackedSequence(probs, decoded.batch_sizes), hiddens
+
+    def forward(self, image, input_captions):
+        image_vector = self.encoder(image)
+        image_vector = image_vector.unsqueeze(0)
+        return self.decoder(image_vector, input_captions)
+
+
+class ResNetLSTM(nn.Module):
+    def __init__(self, dict_size, embedding_dim, hidden_size, *args, **kwargs):
+        super(ResNetLSTM, self).__init__(*args, **kwargs)
+        # TODO: try to use mean instead of flatten all the features
+        self.linear_h = nn.Linear(in_features=100352, out_features=hidden_size)
+        self.linear_c = nn.Linear(in_features=100352, out_features=hidden_size)
+
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(num_embeddings=dict_size, embedding_dim=embedding_dim)
+        self.rnn = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size)
+        self.linear2 = nn.Linear(in_features=hidden_size, out_features=dict_size)
+
+    def encoder(self, image):
+        return self.linear1(image.view(image.shape[0], -1)).view(-1, self.hidden_size)
+
+    def decoder(self, h0, c0, input_captions):
+        embeddings = nn.utils.rnn.PackedSequence(
+            self.embedding(input_captions.data),
+            input_captions.batch_sizes)
+        decoded, (hiddens, cells) = self.rnn(embeddings, (h0, c0))
+        probs = self.linear2(decoded.data)
+        return nn.utils.rnn.PackedSequence(probs, decoded.batch_sizes), hiddens, cells
+
+    def get_h(self, image):
+        h = self.linear_h(image.view(image.shape[0], -1)).view(-1, self.hidden_size)
+        return h
+
+    def get_c(self, image):
+        c = self.linear_c(image.view(image.shape[0], -1)).view(-1, self.hidden_size)
+        return c
+
+    def forward(self, image, input_captions):
+        h0 = self.get_h(image).unsqueeze(0)
+        c0 = self.get_c(image).unsqueeze(0)
+        return self.decoder(h0, c0, input_captions)
