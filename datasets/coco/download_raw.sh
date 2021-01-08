d628e51aa1f4f21c4358bb321681b532c0aa59a7
vitonka <victor.otliga@gmail.com>
2021-01-08 13:44:30 +0000

    Fix datasets repository part structure

--- datasets/coco/download_raw.sh
+++ datasets/coco/download_raw.sh
@@ -1,5 +1,5 @@
-mkdir coco_raw
-cd coco_raw
+mkdir raw
+cd raw
 mkdir images
 cd images
 
@@ -15,7 +15,6 @@ rm train2014.zip
 rm val2014.zip
 rm test2014.zip
 
-
 cd ../
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 wget http://images.cocodataset.org/annotations/image_info_test2014.zip
@@ -25,6 +24,13 @@ unzip image_info_test2014.zip
 
 rm annotations_trainval2014.zip
 rm image_info_test2014.zip
+mv annotations/captions_train2014.json annotations/captions_train2014_raw.json
+mv annotations/captions_val2014.json annotations/captions_val2014_raw.json
+rm annotations/image_info_test2014.json
+rm annotations/instances_train2014.json
+rm annotations/instances_val2014.json
+rm annotations/person_keypoints_train2014.json
+rm annotations/person_keypoints_val2014.json
 
 wget http://cs.stanford.edu/people/karpathy/deepimagesent/caption_datasets.zip
 unzip caption_datasets.zip
