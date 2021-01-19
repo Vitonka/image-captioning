b2d2e135d8e7981126c8b1d2264647a22b1c038d
vitonka <victor.otliga@gmail.com>
2021-01-19 06:19:53 +0000

    Working encoder + attention part of ShowAttendTell

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch
+import torchvision
 
 
 class ShowAndTellWithPretrainedImageEmbeddings(nn.Module):
@@ -122,3 +123,56 @@ class ShowAndTellLSTM(nn.Module):
             _, history = self.lstm(image_vector.unsqueeze(1))
 
         return self.decoder(history, input_captions)
+
+
+class ShowAttendTell(nn.Module):
+    def __init__(
+            self,
+            embedding_dim, hidden_size):
+        super(ShowAttendTell, self).__init__()
+
+        resnet = torchvision.models.resnet101(pretrained=True)
+        resnet.eval()
+        modules = list(resnet.children())[:-2]
+        self.resnet = nn.Sequential(*modules)
+
+        self.linear_h = nn.Linear(in_features=2048, out_features=hidden_size)
+        self.linear_c = nn.Linear(in_features=2048, out_features=hidden_size)
+
+        self.att = nn.Linear(in_features=hidden_size, out_features=49)
+
+        self.hidden_size = hidden_size
+        self.softmax = nn.Softmax(dim=1)
+
+    def encoder(self, image):
+        encoded = self.resnet(image)
+        assert encoded.shape[1:] == (2048, 7, 7), \
+            'Shapes mismatch, actual shape is ' + str(encoded.shape[1:])
+
+        encoded = encoded.reshape(encoded.shape[0], encoded.shape[1], -1)
+        assert encoded.shape[1:] == (2048, 49)
+
+        return encoded
+
+    def attention(self, encoded, hidden):
+        assert encoded.shape[1:] == (2048, 49)
+        assert hidden.shape[1:] == (self.hidden_size,)
+
+        att = self.softmax(self.att(hidden))
+        assert att.shape[1:] == (49,)
+
+        attended = torch.sum(att * encoded, dim=2)
+        assert attended.shape[1:] == (2048,), \
+            'Shapes mismatch, actual shape is ' + str(attended.shape[1:])
+
+        return attended
+
+    def forward(self, image):
+        image_vector = self.encoder(image)
+        image_mean = torch.mean(image_vector, dim=2)
+        assert image_mean.shape[1:] == (2048,)
+
+        h0 = self.linear_h(image_mean)
+        c0 = self.linear_c(image_mean)  # noqa
+
+        attended = self.attention(image_vector, h0)  # noqa
